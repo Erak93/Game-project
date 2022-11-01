cf1208343602c3e9d44057f920a2974d11924992
Erak93 <giannicolaciudino@yahoo.it>
2022-11-01 11:18:10 +0100

    first script commit Gia

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Intro about the world, background story and classes introduction")
+
+race=input("Come closer. I can not really see you well. Who are you?   ")
+race_class=input("What is your fighting style?" "Warrior,Archer or Wizard?   ")
+
+def mycharacter(race,race_class):
+  print("Your race is: ",race)
+  print("Your class is:",race_class)
+  
+
+mycharacter(race,race_class) 
