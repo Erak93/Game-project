9024ddd013418b89a4cd7e38609822e0793bfde3
marynaSolovarova <kimseokjinshi@gmail.com>
2022-11-02 17:50:26 +0200

    messing with import

--- test.py
+++ test.py
@@ -68,6 +68,7 @@ quest_choice = input("Which quest will you choose? 1, 2 or 3 ")
 
 if quest_choice == "1":
     import quest1
+    
     #execfile("\Admin\Desktop\Python\VScode\Exercises-from-Window\Game-project\quest1.py")
        
 
