0c8a5c299e78d34c4e8603d5f2cdbb6815543905
Erak93 <giannicolaciudino@yahoo.it>
2022-11-14 16:54:59 +0100

    commit

--- quest1.py
+++ quest1.py
@@ -6,4 +6,4 @@ import mymodule
 #print("You are back in the middle of the forest. Where do you want to go next")
 #movement=input("Choose East,North,South")
 
-#mymodule.go_south_forest()
+mymodule.go_south_forest()
